dd24de325e764492270d4e192c121ce034da66bb
Sibtain Norain <sibtain.norain@northbaysolutions.net>
2018-01-12 03:41:08 +0500

    - Fixed date comparison issues. - Handled edge cases for date values.

--- app/public/js/controllers/hotelController.js
+++ app/public/js/controllers/hotelController.js
@@ -1,5 +1,4 @@
 'use strict';
-var HOTELS = [];
 
 angular
     .module('hotelSearchApp', ['ui.bootstrap', 'dataGrid', 'angularMoment'])
@@ -11,46 +10,57 @@ angular
 
             $scope.error = false;
             $scope.nights = 0;
-
-            hotelsFactory.getData().then(function (responseData) {
-                HOTELS = responseData.data.hotels;
-            });
+            var hotels = [];
 
             $scope.searchHotels = function (dateFrom, dateTo) {
                 $scope.error = false;
+                $scope.nights = 0;
 
                 if (dateFrom == undefined || dateTo == undefined) {
                     $scope.error = true;
+                    $scope.gridOptions.data = [];
                     return;
                 }
 
                 let fromFilter = moment(dateFrom);
                 let toFilter = moment(dateTo);
 
-                if (fromFilter.isAfter(toFilter) || toFilter.isBefore(fromFilter)) {
+                if (fromFilter.isSame(toFilter) || fromFilter.isAfter(toFilter) || toFilter.isBefore(fromFilter)) {
                     $scope.error = true;
+                    $scope.gridOptions.data = [];
                     return;
                 }
 
-                $scope.nights = toFilter.diff(fromFilter, 'days');
-                let filteredHotels = [];
+                hotelsFactory.getData().then(function (responseData) {
+                    hotels = responseData.data.hotels;
 
-                for (let i = 0; i < HOTELS.length; i++) {
-                    let availability = HOTELS[i].availability;
+                    $scope.nights = toFilter.diff(fromFilter, 'days');
+                    let filteredHotels = [];
 
-                    for (let j = 0; j < availability.length; j++) {
-                        let availabilityFrom = moment(availability[j].from);
-                        let availabilityTo = moment(availability[j].to);
+                    for (let i = 0; i < hotels.length; i++) {
+                        let availability = hotels[i].availability;
 
-                        if (fromFilter.isBetween(availabilityFrom, availabilityTo, null, '[]') && toFilter.isBetween(availabilityFrom, availabilityTo, null, '[]')) {
-                            console.log(j);
-                            console.log(HOTELS[i].name);
-                            filteredHotels.push(HOTELS[i]);
-                            break;
+                        for (let j = 0; j < availability.length; j++) {
+                            let availabilityFrom = moment(availability[j].from, "DD-MM-YYYY");
+                            let availabilityTo = moment(availability[j].to, "DD-MM-YYYY");
+
+                            console.log(fromFilter);
+                            console.log(availabilityFrom);
+                            
+                            console.log(toFilter);
+                            console.log(availabilityTo);
+                            
+                            if (fromFilter.isBetween(availabilityFrom, availabilityTo, 'days', '[]') && toFilter.isBetween(availabilityFrom, availabilityTo, 'days', '[]')) {
+                                hotels[i].price = hotels[i].price * $scope.nights;
+                                console.log(j);
+                                console.log(hotels[i].name);
+                                filteredHotels.push(hotels[i]);
+                                break;
+                            }
                         }
                     }
-                }
-                $scope.gridOptions.data = filteredHotels;
+                    $scope.gridOptions.data = filteredHotels;
+                });
             };
         }])
     .factory('hotelsFactory', function ($http) {
