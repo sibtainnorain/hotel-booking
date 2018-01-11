15ae3bd87c83a0355cb1315d5007a9b124ea7220
Sibtain Norain <sibtain.norain@northbaysolutions.net>
2018-01-12 03:06:53 +0500

    - Completed the implementation of all features. - Fixed date related issues.

--- app/public/js/controllers/hotelController.js
+++ app/public/js/controllers/hotelController.js
@@ -1,22 +1,65 @@
-angular.module('hotelSearchApp', ['ui.bootstrap', 'dataGrid', 'pagination'])
-.controller('hotelsController', ['$scope', 'hotelsFactory', '$filter', function ($scope, hotelsFactory, $filter) {
+'use strict';
+var HOTELS = [];
 
-    $scope.gridOptions = {
-        data: [],
-        urlSync: true
-    };
+angular
+    .module('hotelSearchApp', ['ui.bootstrap', 'dataGrid', 'angularMoment'])
+    .controller('hotelsController', ['$scope', 'hotelsFactory', '$filter',
+        function ($scope, hotelsFactory, $filter) {
+            $scope.gridOptions = {
+                data: [],
+            };
 
-    hotelsFactory.getData().then(function (responseData) {
-        $scope.gridOptions.data = responseData.data.hotels;
-    });
-}])
-.factory('hotelsFactory', function ($http) {
-    return {
-        getData: function () {
-            return $http({
-                method: 'GET',
-                url: 'https://api.myjson.com/bins/tl0bp'
+            $scope.error = false;
+            $scope.nights = 0;
+
+            hotelsFactory.getData().then(function (responseData) {
+                HOTELS = responseData.data.hotels;
             });
+
+            $scope.searchHotels = function (dateFrom, dateTo) {
+                $scope.error = false;
+
+                if (dateFrom == undefined || dateTo == undefined) {
+                    $scope.error = true;
+                    return;
+                }
+
+                let fromFilter = moment(dateFrom);
+                let toFilter = moment(dateTo);
+
+                if (fromFilter.isAfter(toFilter) || toFilter.isBefore(fromFilter)) {
+                    $scope.error = true;
+                    return;
+                }
+
+                $scope.nights = toFilter.diff(fromFilter, 'days');
+                let filteredHotels = [];
+
+                for (let i = 0; i < HOTELS.length; i++) {
+                    let availability = HOTELS[i].availability;
+
+                    for (let j = 0; j < availability.length; j++) {
+                        let availabilityFrom = moment(availability[j].from);
+                        let availabilityTo = moment(availability[j].to);
+
+                        if (fromFilter.isBetween(availabilityFrom, availabilityTo, null, '[]') && toFilter.isBetween(availabilityFrom, availabilityTo, null, '[]')) {
+                            console.log(j);
+                            console.log(HOTELS[i].name);
+                            filteredHotels.push(HOTELS[i]);
+                            break;
+                        }
+                    }
+                }
+                $scope.gridOptions.data = filteredHotels;
+            };
+        }])
+    .factory('hotelsFactory', function ($http) {
+        return {
+            getData: function () {
+                return $http({
+                    method: 'GET',
+                    url: 'https://api.myjson.com/bins/tl0bp'
+                });
+            }
         }
-    }
-});
+    });
--- server.js
+++ server.js
@@ -4,12 +4,11 @@ var path = require('path');
 var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var morgan = require('morgan');
 
 app.use(express.static('./app/public'));
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(morgan('dev'));
 
 require('./app/routes.js')(app);
 
